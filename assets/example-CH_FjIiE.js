dce13a5fb76174fc67ef9d4c631f4fc38fb5e7b5
hung.pv <hung.pv@eofactory.ai>
2024-06-18 17:59:35 +0700

    chore(link): change link

---
+++ assets/example-CH_FjIiE.js
@@ -0,0 +1,1 @@
+import{d as s,c as n,w as r,u as a,o as p,a as t,D as c,B as i,M as l,b as _}from"./index-afu2ryHI.js";const m=s({__name:"example",setup(u){const e={draw_support:[_.POINT],async save(o){console.log("save",o)}};return(o,d)=>(p(),n(a(l),null,{default:r(()=>[t(a(c),{position:"top-right",initShow:"",drawOptions:e}),t(a(i),{position:"bottom-left"})]),_:1}))}});export{m as default};
